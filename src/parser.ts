834f9ff2b09ede39887104405fdda029658bdbcb
Peter-Michael Osera <psosera@gmail.com>
2022-08-16 15:16:26 -0500

    Checkpoint on debugging ceiling

--- src/parser.ts
+++ src/parser.ts
@@ -91,9 +91,15 @@ function sexpToExp (s: Sexp): Result<Exp> {
     case 'atom': {
       // TODO: check to see if the atom is a number, character, boolean, or string
       // TODO: number parsing is complicated, this is a quick hack to move forward
+      // BUG: need to parse a double, too!
+      // TODO: just need to regex this... -_-
       const asInt = parseInt(s.single, 10)
       if (!isNaN(asInt)) {
         return ok(enumber(s.range, asInt))
+      }
+      const asFloat = parseFloat(s.single)
+      if (!isNaN(asFloat)) {
+        return ok(enumber(s.range, asFloat))
       } else if (s.single.startsWith('"')) {
         const result = tryParseString(s.single)
         return result
