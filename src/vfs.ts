b07c19af6a9d70388a0b29e49eb25be40a4f30b2
Peter-Michael Osera <psosera@gmail.com>
2022-09-30 07:06:39 -0500

    web hack

--- src/vfs.ts
+++ src/vfs.ts
@@ -42,6 +42,19 @@ class VFS {
   }
 
   read(path: string): Result<string> {
+    // TODO: hack to quickly get in https pull-down support for files!
+    if (path.startsWith('http://') || path.startsWith('https://')) {
+      console.log('...')
+      const request = new XMLHttpRequest();
+      request.open('GET', path, false);  // `false` makes the request synchronous
+      request.send(null);
+      if (request.status === 200) {
+        return ok(request.responseText);
+      } else {
+        throw new ICE('VFS.read', 'request didnt work!')
+      }
+    }
+
     for (const [prefix, provider] of this.mountPoints.entries()) {
       if (path.startsWith(prefix)) {
         return provider.read(path.substring(prefix.length));
